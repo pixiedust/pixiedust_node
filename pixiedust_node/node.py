c9d142f334962c590007bbc53b9e5c24e86fc35e
Glynn Bird <glynn.bird@gmail.com>
2017-06-15 14:24:13 +0100

    detect missing node

--- pixiedust_node/node.py
+++ pixiedust_node/node.py
@@ -1,5 +1,6 @@
 
 import subprocess
+import os
 from .nodestdreader import NodeStdReader
 
 # runs a Node sub-process and starts a NodeStdReader thread
@@ -11,13 +12,41 @@ class Node:
 
     # run a JavaScript script (path) with "node"
     def __init__(self, path):
+        # get the home directory
+        node = 'node';
+        home = get_ipython().home_dir
 
-        # create sub-process
-        self.ps = subprocess.Popen( ('node', path), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        #print ("Node process id", self.ps.pid)
+        # check that node exists
+        node_path = self.which(node)
 
-        # create thread to read this process's output          
-        t = NodeStdReader(self.ps)
+        if node_path == None:
+            print 'ERROR: Cannot find Node.js executable'
+            raise FileNotFoundError('node executable not found in path')
+        else:
+            # create sub-process
+            self.ps = subprocess.Popen( (node_path, path), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd = home)
+            # print ("Node process id", self.ps.pid)
+
+            # create thread to read this process's output          
+            t = NodeStdReader(self.ps)
+
+
+    def is_exe(self, fpath):
+       return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    def which(self, program):
+        fpath, fname = os.path.split(program)
+        if fpath:
+            if self.is_exe(program):
+                return program
+        else:
+            for path in os.environ["PATH"].split(os.pathsep):
+                path = path.strip('"')
+                exe_file = os.path.join(path, program)
+                if self.is_exe(exe_file):
+                    return exe_file
+
+        return None
 
     def write(self, s):
         self.ps.stdin.write(s)
