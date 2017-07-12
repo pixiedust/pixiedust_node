f894c9f8e7d4cd957057148a28b0e5ab74a0c9e5
Glynn Bird <glynn.bird@gmail.com>
2017-07-12 08:03:34 +0100

    0.1.1 include .js and .json files in the package

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='pixiedust_node',
-      version='0.1',
+      version='0.1.1',
       description='Pixiedust extension for Node.js',
       url='https://github.com/ibm-cds-labs/pixiedust_node',
       install_requires=['pixiedust'],
+      package_data={
+        '': ['*.js','*.json']
+      },
       author='David Taieb, Glynn Bird',
       author_email='david_taieb@us.ibm.com, glynn.bird@gmail.com',
       license='Apache 2.0',
