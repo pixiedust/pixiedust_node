708b34086b7a7ab4568ac6f8b7660045bfb1336f
Glynn Bird <glynn.bird@gmail.com>
2018-01-05 09:17:59 +0000

    0.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pixiedust_node',
-      version='0.1.6',
+      version='0.1.7',
       description='Pixiedust extension for Node.js',
       url='https://github.com/ibm-watson-data-lab/pixiedust_node',
       install_requires=['pixiedust', 'pandas', 'ipython'],
